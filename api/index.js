b814693e6ed0141e25d02789898d6dd728fee7d1
Endel Dreyer <endel.dreyer@gmail.com>
2022-01-22 11:44:12 -0300

    fixes to use vercel api

--- api/index.js
+++ api/index.js
@@ -1,36 +1,22 @@
-require('dotenv').config();
+const { TodoistApi } = require('@doist/todoist-api-typescript')
 
-import Fastify from 'fastify'
-import fastifyStatic from 'fastify-static';
-import path from 'path'
-import { TodoistApi } from '@doist/todoist-api-typescript'
+const TODOIST_TOKEN = process.env.TODOIST_TOKEN;
+const TODOIST_PROJECT_ID = process.env.TODOIST_PROJECT_ID;
 
-const api = new TodoistApi(process.env.TODOIST_TOKEN);
-const fastify = Fastify({ logger: true })
+module.exports = async (request, response) => {
+  if (!TODOIST_TOKEN || !TODOIST_PROJECT_ID) {
+    response.send([
+      { content: "Make sure to set TODOIST_TOKEN and TODOIST_PROJECT_ID environment variables." }
+    ]);
+    return;
+  }
 
-fastify.register(fastifyStatic, {
-  root: path.join(__dirname, "..", "public"),
-  prefix: '/public/',
-})
-
-// Declare a route
-fastify.get('/', async (request, reply) => {
-  return reply.sendFile('index.html');
-});
-
-fastify.get('/Typo_Hoop_Regular.otf', async (request, reply) => {
-  return reply.sendFile('Typo_Hoop_Regular.otf');
-});
-
-// Declare a route
-fastify.get('/api', async (request, reply) => {
+  const api = new TodoistApi(process.env.TODOIST_TOKEN);
   const tasks = (await api.getTasks()).
     filter((task) => task.projectId === 2261610951).
     // filter((task) => !task.completed).
     sort((a, b) => (a.order <= b.order) ? -1 : 1).
     sort((a, b) => (a.priority <= b.priority) ? 1 : -1);
 
-  reply.send(tasks);
-});
-
-fastify.listen(process.env.PORT || 8888);
+  response.send(tasks);
+};
